c74695871d7d859835bfd21a1730e10e9cdd10fb
Andrew Li <andrewli357@gmail.com>
2023-02-08 23:45:51 -0500

    Checkpoint 3: Get some bounce

--- App.js
+++ App.js
@@ -6,17 +6,21 @@ import cookieImage from './assets/cookie.png';
 
 export default function App() {
   const [clicks, setClicks] = useState(0);
+  const [clicking, setClicking] = useState(false);
 
   const handlePress = () => {
     setClicks(clicks + 1);
   }
 
+  const cookieSize = clicking ? 180 : 200;
+  const cookieStyle = { width: cookieSize, height: cookieSize };
+
   return (
     <View style={styles.container}>
       <Text>Cookie Clicker 2</Text>
       <Text>Clicks: {clicks}</Text>
-      <Pressable onPress={handlePress}>
-        <Image source={cookieImage} style={styles.cookie} />
+      <Pressable onPress={handlePress} onPressIn={() => setClicking(true)} onPressOut={() => setClicking(false)}>
+        <Image source={cookieImage} style={cookieStyle} />
       </Pressable>
       <StatusBar style="auto" />
     </View>
@@ -29,9 +33,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  cookie: {
-    width: 200,
-    height: 200,
   }
 });
